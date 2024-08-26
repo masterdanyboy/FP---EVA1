729deb141e645eca7a8aa8f1595e83c03663f768
masterdanyboy <danniiel.dn@gmail.com>
2024-08-26 17:53:51 -0600

    Add files via upload

---
+++ EVA_1_3_VARIABLES2/src/eva_1_3_variables2/EVA_1_3_VARIABLES2.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_3_variables2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA_1_3_VARIABLES2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //VEHICULOS:
+        String marca;
+        String modelo;
+        int año;//la ñ en otros lenguajes no es aceptada
+        double precio; 
+        
+        //ASIGNACIÓN
+        marca = "NISSAN";
+        modelo = "GTR";
+        año = 2008;
+        precio = 1000000.1;
+        
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(año);
+        System.out.println(precio);
+    }
+    
+}
