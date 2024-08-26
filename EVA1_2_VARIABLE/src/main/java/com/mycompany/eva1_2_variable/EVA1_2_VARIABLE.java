13f1fc874ab100bc2501eda78a09f06d0546fd5d
masterdanyboy <danniiel.dn@gmail.com>
2024-08-26 15:20:40 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLE/src/main/java/com/mycompany/eva1_2_variable/EVA1_2_VARIABLE.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_variable;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_2_VARIABLE {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hola mundo");   //cadena de texto
+        System.out.println(100);   //Entero
+        System.out.println(3.1416); //Número con punto flotante
+        System.out.println('c');    //caracter
+        System.out.println(true);   //valores lógicos (booleanos)
+        System.out.println(
+                false
+        )
+                ;//; -- FIN DE INSTRUCCIÓN (NO SE LES OLVIDE PONERLO
+        //ENETEROS
+        //int
+        //long
+        //short
+        
+        //NUMEROS CON PARTE DECIMAL (FLOTANTES)
+        //float
+        //double
+        
+        //CADENAS DE TEXTO
+        //string
+        //
+        //CARACTERES
+        //char
+        
+        //VALORES LÓGICOS
+        //boolean
+        
+       //DECLARACIÓN DE VARIABLES:
+       //SYNTAXIS PARA DECLARAR VARIABLES (REGLAS)
+       //TIPO DE DATO NOMBRE DE LA VARIABLE;
+       String nombre;
+       String apellido;
+       String curp;
+       float promedio; 
+       System.out.println(100
+       System.out.print(true);      //boolean
+    }
+}
