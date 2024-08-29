8b30d9fb32c0fa769ce99d48a69336873b34a7a2
masterdanyboy <danniiel.dn@gmail.com>
2024-08-29 16:21:04 -0600

    Add files via upload

---
+++ EVA1_4_TV/src/eva1_4_tv/EVA1_4_TV.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_tv;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_4_TV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //TELEVISIONES
+        String marca;
+        String tipo;
+        String tecnología;
+        int tamaño;
+        double modelo;
+        double precio;
+        
+        
+        //ASIGNACIÓN
+        marca = "LG";
+        tipo = "EVO";
+        tamaño = 55;
+        modelo = 2024;
+        precio = 29999;
+        tecnología = "OLED";
+              
+        System.out.println(marca);
+        System.out.println(tipo);
+        System.out.println(tamaño);
+        System.out.println(modelo);
+        System.out.println(precio);
+        System.out.println(tecnología);
+        
+        
+        
+    }
+    
+}
