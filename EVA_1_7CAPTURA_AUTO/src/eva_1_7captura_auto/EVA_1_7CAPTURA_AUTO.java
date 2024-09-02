a8f32e80d5fc4b4554c48b68e25e6dd793a2cc33
masterdanyboy <danniiel.dn@gmail.com>
2024-09-01 20:51:05 -0600

    Add files via upload

---
+++ EVA_1_7CAPTURA_AUTO/src/eva_1_7captura_auto/EVA_1_7CAPTURA_AUTO.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_7captura_auto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA_1_7CAPTURA_AUTO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //C
+        String marca;
+        String modelo;
+        int año;
+        double precio;
+        //CREAR NUESTRO SCANNER
+        Scanner captu;
+        captu = new Scanner(System.in);
+        
+        //CAPTURAR
+        System.out.println("Captura la marca:");
+        marca = captu.nextLine();
+        
+        System.out.println("Captura el modelo");
+        modelo = captu.nextLine();
+        
+        System.out.println("Captura el año");
+        año = captu.nextInt();
+        
+        System.out.println("Captura el precio");
+        precio = captu.nextDouble();
+       s
+        
+        
+        
+        
+    }
+    
+}
