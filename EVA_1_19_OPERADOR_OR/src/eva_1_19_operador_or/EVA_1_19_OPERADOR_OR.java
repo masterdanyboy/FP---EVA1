338b0db80a14973ebe1be0a98e633944a978c966
masterdanyboy <danniiel.dn@gmail.com>
2024-10-05 20:33:26 -0600

    Add files via upload

--- EVA_1_19_OPERADOR_OR/src/eva_1_19_operador_or/EVA_1_19_OPERADOR_OR.java
+++ EVA_1_19_OPERADOR_OR/src/eva_1_19_operador_or/EVA_1_19_OPERADOR_OR.java
@@ -17,16 +17,24 @@ public class EVA_1_19_OPERADOR_OR {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int km, meses;
-        
-        Scanner captu = new Scanner (System.in);
-        System.out.println ("Imprime el kilometraje: ");
-        km = captu.nextInt ();
+        int Kilometraje;
+        int meses;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Introduce el kilometraje del auto: ");
+        Kilometraje = captu.nextInt();
         captu.nextLine();
-        System.out.println ("Imprime los meses: ");
-        meses = captu.nextInt ();
+        System.out.println("Introduce los meses que han pasado desde el ultimo cambio de aceite: ");
+        meses = captu.nextInt();
         captu.nextLine();
-        if ((km >= 500 || meses
+
+
+        if(Kilometraje >= 5000 || meses >= 6){
+            System.out.println("Cambio de aceite");
+            }
+        else{
+
+            System.out.println("No es necesario cambio de aceite");
     }
     
+}
 }
