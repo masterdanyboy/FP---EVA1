101fc2a58f43933725e15a39f53b86af54edd68c
masterdanyboy <danniiel.dn@gmail.com>
2024-10-05 18:46:18 -0600

    Add files via upload

---
+++ EVA_1_24_CALIFAS/src/eva_1_24_califas/EVA_1_24_CALIFAS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_24_califas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA_1_24_CALIFAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
